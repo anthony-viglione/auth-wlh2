6200e92b860d033d32f8fa790f362ffce5fdf2a8
Mark McIver <mark@Marks-MacBook-Pro.local>
2019-02-27 12:22:34 -0700

    server-side for auth

---
+++ server/controllers/Auth.js
@@ -0,0 +1,76 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  signup: async (req, res) => {
+    // user inputs their info: name, email, password
+    // check to see if email already in db
+    // create a salt
+    // create a hash from password and salt
+    // create record for user in db
+    // add user to the session
+    try {
+      const db = req.app.get('db')
+      let { name, email, password } = req.body
+  
+      let userResponse = await db.findUserByEmail(email)
+      let user = userResponse[0]
+  
+      if (user) {
+        return res.status(409).send('email already used')
+      }
+  
+      const salt = bcrypt.genSaltSync(10)
+      const hash = bcrypt.hashSync(password, salt)
+  
+      let createdUser = await db.createUser({name, email, hash})
+      req.session.user = createdUser[0]
+      res.send(req.session.user)
+    } catch (error) {
+      console.log('error signing up user', error)
+      res.status(500).send(error)
+    }
+  },
+
+  login: async (req, res) => {
+    try {
+      // user inputs info: email, password
+      // get user from db
+      // if no user, send 409 status
+      // compare the password and the hash using bcrypt
+      // if they don't match send 401
+      // save user on session
+      let db = req.app.get('db')
+      let { email, password } = req.body
+
+      let userResponse = await db.findUserByEmail(email)
+      let user = userResponse[0]
+
+      if (!user) {
+        return res.status(409).send('email not found')
+      }
+
+      const isAuthenticated = bcrypt.compareSync(password, user.password)
+
+      if (!isAuthenticated) {
+        return res.status(401).send('incorrect password')
+      }
+
+      delete user.password
+
+      req.session.user = user
+      res.send(user)
+    } catch (error) {
+      console.log('error loging in user', error)
+      res.status(500).send(error)
+    }
+  },
+
+  logout: (req, res) => {
+    req.session.destroy()
+    res.sendStatus(200)
+  },
+
+  current: (req, res) => {
+    res.send(req.session.user)
+  }
+}
